e6aac6f24eafdce9c89e8dccd2e3ea19d46b471f
Yuki M <yukiji.0307@gmail.com>
2022-01-14 08:45:10 +0900

    実験中

--- src/tsx/app.tsx
+++ src/tsx/app.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import json from '../data/data.json'
 
 const Hello = () => {
   return (
     <>
       <h2>Hello React</h2>
+      <p>{json.name}</p>
+      <p>{json.age}</p>
     </>
   )
 }
