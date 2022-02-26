c61beec321ced263610bffa0ccc541c0f6e2bcc0
ykj-rails <yukiji.0307@gmail.com>
2022-02-26 13:09:05 +0900

    ファイル整理

---
+++ src/components/page/Top/Top.page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Top } from './Top'
+
+import { Header } from '@/components/common'
+import { Footer } from '@/components/common'
+
+export const TopPage = () => {
+  return (
+    <>
+      <Header />
+      <Top />
+      <Footer />
+    </>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+import { TopPage } from '@/components/page/Top'
+
+export default TopPage
