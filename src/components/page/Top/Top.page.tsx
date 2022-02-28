afb550b5c95ea75d3193c03d9993ca3ec8fa2132
ykj-rails <yukiji.0307@gmail.com>
2022-03-01 08:14:43 +0900

    [fix] import変更

--- src/components/page/Top/Top.page.tsx
+++ src/components/page/Top/Top.page.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { Top } from './Top'
 
-import { Header } from '@/components/common'
-import { Footer } from '@/components/common'
+import { Header, Footer } from '@/components/common'
 
 export const TopPage = () => {
   return (
