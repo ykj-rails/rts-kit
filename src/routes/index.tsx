9dd371570f151a63045cb6aff2ddaa11fa25cddd
松本雄揮 <yukiji.0307@gmail.com>
2022-04-26 15:57:47 +0900

    [update] CodeSplittingのためにdynamic import x React.Lazy

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import TopPage from '@/pages'
+const TopPage = lazy(() => import('@/pages'))
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<TopPage />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<TopPage />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   )
 }
