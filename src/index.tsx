bae92f1bf3d0c07893a04b9fc0691745449f5dd4
松本雄揮 <yukiji.0307@gmail.com>
2022-04-27 12:10:26 +0900

    [fix] log消す

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom'
 
 import App from './App'
 
-console.log('aaa')
-
 if (process.env.NODE_ENV === 'development') {
   const { worker } = await import('./mocks/browser')
   worker.start({
